378f54a3b517aff5ae1499cd5f4b7a05eba61ab7
Nishant Ranjan <82319394+nishantranjan127@users.noreply.github.com>
2023-03-16 01:14:22 +0530

    Create 9.cpp

---
+++ DPP1/9.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define all(x) x.begin(),x.end()
+#define vl vector<ll>
+#define fr(v) for(ll i=0;i<v.size();i++)
+void solve()
+{
+ ll i,j,a,b=1,c=0,d,e,f,g,h,k,l,n,o=0,sum=0,count=1,cnt=0,ans=0;
+
+ 
+ cin>>a;
+ if(a%4==0)
+        cout<< "false";
+        cout<<"true";
+ ;
+}
+int main()
+{
+ 
+ ll t,y=1;
+ cin>>t;
+ while(t--) 
+ { 
+ solve();
+ 
+ } 
+ 
+ return 0;
+}
